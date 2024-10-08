1d92bd548a7eff0f2a3c6cda485b4bfc8bb052be
N1ck120 <nickolasaugusto74@gmail.com>
2024-10-08 12:35:54 -0300

    Criação do App Lista de compras

---
+++ ListadeCompras/app/src/main/java/com/n1ck120/listadecompras/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.n1ck120.listadecompras
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val editInsert = findViewById<EditText>(R.id.editTextItem)
+        val btnInsert = findViewById<Button>(R.id.buttonInsert)
+        val listItens = findViewById<ListView>(R.id.listViewItem)
+
+        val itensAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+
+        btnInsert.setOnClickListener {
+            if(editInsert.text.isEmpty()){
+                editInsert.error = "Campo Obrigatorio"
+            }else{
+                val produto = editInsert.text.toString()
+                itensAdapter.add(produto)
+                editInsert.text.clear()
+            }
+        }
+    }
+}
