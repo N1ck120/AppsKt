60cb93ebf9179700155cd8bb5045d928a477957e
N1ck120 <nickolasaugusto74@gmail.com>
2024-11-13 09:11:52 -0300

    Criação do app cotação bitcoin

---
+++ CotaoBitcoin/app/src/main/java/com/n1ck120/cotaobitcoin/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.n1ck120.cotaobitcoin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import java.net.URL
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.Main
+import org.json.JSONObject
+import java.text.NumberFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val API_URL = "https://api.hgbrasil.com/finance"
+        var cotacao = 0.0
+
+        buscarcotacao()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
